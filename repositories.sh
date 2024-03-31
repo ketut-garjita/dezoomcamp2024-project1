df3d6dfd020bda26e3cb2d5a004e61e0dbfbcae0
Ketut Garjita <77673886+garjita63@users.noreply.github.com>
2024-03-31 20:59:19 +0700

    Update repositories.sh

--- repositories.sh
+++ repositories.sh
@@ -1,19 +1,3 @@
-#############Install Docker#############
-# Update the apt package index and install packages to allow apt to use a repository over HTTPS
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
-
-# The following command is to set up the stable repository
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` stable" 
-
-# Update the apt package index, and install the latest version of Docker Engine and containerd, or go to the next step to install a specific version
-sudo apt update 
-sudo apt install -y docker-ce
-
-
 #############Install Docker network#############
 #create a network most containers will use
 sudo docker network create dockernet >> /root/dockernet.log
@@ -27,38 +11,38 @@ version: '3'
 services:
   magic:
     image: mageai/mageai:latest
-    command: mage start dezoomcamp
-    container_name: dezoomcamp-mage
+    command: mage start <some_folder>
+    container_name: <some_folder>-mage
     build:
       context: .
       dockerfile: Dockerfile
     environment:
-      USER_CODE_PATH: /home/src/dezoomcamp
-      POSTGRES_DBNAME: dezoomcampdb
+      USER_CODE_PATH: /home/src/<some_folder>
+      POSTGRES_DBNAME: <some_folder>db
       POSTGRES_SCHEMA: public
       POSTGRES_USER: postgres
       POSTGRES_PASSWORD: postgres316
-      POSTGRES_HOST: vm-ikg-dezoomcamp
+      POSTGRES_HOST: vm-ikg-<some_folder>
       POSTGRES_PORT: 5432
     ports:
       - 6789:6789
     volumes:
       - .:/home/src/
-      - /root/.google/credentials/key-ikg-dezoomcamp-2024.json
+      - /root/.google/credentials/key-ikg-<some_folder>-2024.json
     restart: on-failure:5
   postgres:
     image: postgres:14
     restart: on-failure
-    container_name: dezoomcamp-postgres
+    container_name: <some_folder>-postgres
     environment:
-      POSTGRES_DB: dezoomcampdb
+      POSTGRES_DB: <some_folder>db
       POSTGRES_USER: postgres
       POSTGRES_PASSWORD: postgres316
     ports:
       - 5432:5432
   pgadmin:
     image: dpage/pgadmin4
-    container_name: dezoomcamp-pgadmin
+    container_name: <some_folder>-pgadmin
     environment:
       - PGADMIN_DEFAULT_EMAIL=admin@admin.com
       - PGADMIN_DEFAULT_PASSWORD=root
@@ -68,9 +52,3 @@ services:
 SCRIPT
 
 sudo docker compose -f /root/docker-compose.yml up -d
-
-
-#############Install some packages############
-sudo apt update
-sudo apt install net-tools
-sudo apt install vim
