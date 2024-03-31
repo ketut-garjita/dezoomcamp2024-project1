96dfc0f9dc7fa56b0fcb997ac85ae8aad078069f
Ketut Garjita <77673886+garjita63@users.noreply.github.com>
2024-03-31 20:50:17 +0700

    Update kaggle-key.sh

--- kaggle-key.sh
+++ kaggle-key.sh
@@ -3,14 +3,14 @@ import json
 
 # Define the content of the kaggle.json file
 data = {
-    "username": "ketutgarjita",
-    "key": "105b7cfe0e35251e4f61267478ae09f3"
+    "username": "<kaggle username>",
+    "key": "<kaggle ke>y"
 }
 
 # Convert the data to a JSON string
 json_string = json.dumps(data)
 
 # Put the JSON string into the /home/src
-file1 = open('/home/smrhitam/code/kaggle.json', 'w')
+file1 = open('/home/<somefolder>/code/kaggle.json', 'w')
 file1.write(json_string)
 file1.close()
