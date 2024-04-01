1ca62c6ba563dd5dff5450468ca005dca99647e4
Ketut Garjita <77673886+garjita63@users.noreply.github.com>
2024-04-02 05:21:59 +0700

    Update repositories.sh

--- repositories.sh
+++ repositories.sh
@@ -11,41 +11,41 @@ version: '3'
 services:
   magic:
     image: mageai/mageai:latest
-    command: mage start <some_folder>
-    container_name: <some_folder>-mage
+    command: mage start dezoomcamp
+    container_name: dezoomcamp-mage
     build:
       context: .
       dockerfile: Dockerfile
     environment:
-      USER_CODE_PATH: /home/src/<some_folder>
-      POSTGRES_DBNAME: <some_folder>db
+      USER_CODE_PATH: /home/src/dezoomcamp
+      POSTGRES_DBNAME: dezoomcampdb
       POSTGRES_SCHEMA: public
       POSTGRES_USER: postgres
-      POSTGRES_PASSWORD: postgres316
-      POSTGRES_HOST: vm-ikg-<some_folder>
+      POSTGRES_PASSWORD: <password>
+      POSTGRES_HOST: <hostname>
       POSTGRES_PORT: 5432
     ports:
       - 6789:6789
     volumes:
       - .:/home/src/
-      - /root/.google/credentials/key-ikg-<some_folder>-2024.json
+      - /root/.google/credentials/gcp-key.json
     restart: on-failure:5
   postgres:
     image: postgres:14
     restart: on-failure
-    container_name: <some_folder>-postgres
+    container_name: dezoomcamp-postgres
     environment:
-      POSTGRES_DB: <some_folder>db
+      POSTGRES_DB: dezoomcampdb
       POSTGRES_USER: postgres
-      POSTGRES_PASSWORD: postgres316
+      POSTGRES_PASSWORD: <password>
     ports:
       - 5432:5432
   pgadmin:
     image: dpage/pgadmin4
-    container_name: <some_folder>-pgadmin
+    container_name: dezoomcamp-pgadmin
     environment:
       - PGADMIN_DEFAULT_EMAIL=admin@admin.com
-      - PGADMIN_DEFAULT_PASSWORD=root
+      - PGADMIN_DEFAULT_PASSWORD=<password>
     ports:
       - 8080:80
       
